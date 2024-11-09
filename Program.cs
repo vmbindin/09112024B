684e6df94980d28110e2f7a00875a67bf03da445
Muhammad Burhan Bin Din (Centific Technologies inc) <v-mbindin@microsoft.com>
2024-11-10 00:03:43 +0800

    Example Task Part 9

--- Program.cs
+++ Program.cs
@@ -2,26 +2,37 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-public class ContinuationTwo
+public class Child
 {
    public static void Main()
    {
-        var outer = Task.Factory.StartNew(() =>
-        {
-            Console.WriteLine("Outer task beginning.");
+      var parent = Task.Factory.StartNew(() => {
+                      Console.WriteLine("Parent task beginning.");
+                      for (int ctr = 0; ctr < 10; ctr++) {
+                         int taskNo = ctr;
+                         Task.Factory.StartNew((x) => {
+                                                  Thread.SpinWait(5000000);
+                                                  Console.WriteLine("Attached child #{0} completed.",
+                                                                    x);
+                                               },
+                                               taskNo, TaskCreationOptions.AttachedToParent);
+                      }
+                   });
 
-            var child = Task.Factory.StartNew(() =>
-            {
-                Thread.SpinWait(5000000);
-                Console.WriteLine("Detached task completed.");
-            });
-        });
-
-        outer.Wait();
-        Console.WriteLine("Outer task completed.");
-        // The example displays the following output:
-        //    Outer task beginning.
-        //    Outer task completed.
-        //    Detached task completed.
+      parent.Wait();
+      Console.WriteLine("Parent task completed.");
    }
 }
+// The example displays output like the following:
+//       Parent task beginning.
+//       Attached child #9 completed.
+//       Attached child #0 completed.
+//       Attached child #8 completed.
+//       Attached child #1 completed.
+//       Attached child #7 completed.
+//       Attached child #2 completed.
+//       Attached child #6 completed.
+//       Attached child #3 completed.
+//       Attached child #5 completed.
+//       Attached child #4 completed.
+//       Parent task completed.
