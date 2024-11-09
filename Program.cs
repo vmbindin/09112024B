c5ad6f620e63c907aebb83bed82c7c50abffa910
Muhammad Burhan Bin Din (Centific Technologies inc) <v-mbindin@microsoft.com>
2024-11-09 23:55:02 +0800

    Example Task 6

--- Program.cs
+++ Program.cs
@@ -1,45 +1,36 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 
-class CustomData
-{
-   public long CreationTime;
-   public int Name;
-   public int ThreadNum;
-}
-
-public class IterationOne
+public class ContinuationOne
 {
    public static void Main()
    {
-      // Create the task object by using an Action(Of Object) to pass in custom data
-      // to the Task constructor. This is useful when you need to capture outer variables
-      // from within a loop.
-      Task[] taskArray = new Task[10];
-      for (int i = 0; i < taskArray.Length; i++) {
-         taskArray[i] = Task.Factory.StartNew( (Object? obj ) => {
-                                                  CustomData? data = obj as CustomData;
-                                                  if (data == null)
-                                                     return;
+      var getData = Task.Factory.StartNew(() => {
+                                             Random rnd = new Random();
+                                             int[] values = new int[100];
+                                             for (int ctr = 0; ctr <= values.GetUpperBound(0); ctr++)
+                                                values[ctr] = rnd.Next();
 
-                                                  data.ThreadNum = Thread.CurrentThread.ManagedThreadId;
-                                                  Console.WriteLine("Task #{0} created at {1} on thread #{2}.",
-                                                                   data.Name, data.CreationTime, data.ThreadNum);
-                                               },
-                                               new CustomData() {Name = i, CreationTime = DateTime.Now.Ticks} );
-      }
-      Task.WaitAll(taskArray);
+                                             return values;
+                                          } );
+      var processData = getData.ContinueWith((x) => {
+                                                int n = x.Result.Length;
+                                                long sum = 0;
+                                                double mean;
+
+                                                for (int ctr = 0; ctr <= x.Result.GetUpperBound(0); ctr++)
+                                                   sum += x.Result[ctr];
+
+                                                mean = sum / (double) n;
+                                                return Tuple.Create(n, sum, mean);
+                                             } );
+      var displayData = processData.ContinueWith((x) => {
+                                                    return String.Format("N={0:N0}, Total = {1:N0}, Mean = {2:N2}",
+                                                                         x.Result.Item1, x.Result.Item2,
+                                                                         x.Result.Item3);
+                                                 } );
+      Console.WriteLine(displayData.Result);
    }
 }
-// The example displays output like the following:
-//       Task #0 created at 635116412924597583 on thread #3.
-//       Task #1 created at 635116412924607584 on thread #4.
-//       Task #3 created at 635116412924607584 on thread #4.
-//       Task #4 created at 635116412924607584 on thread #4.
-//       Task #2 created at 635116412924607584 on thread #3.
-//       Task #6 created at 635116412924607584 on thread #3.
-//       Task #5 created at 635116412924607584 on thread #4.
-//       Task #8 created at 635116412924607584 on thread #4.
-//       Task #7 created at 635116412924607584 on thread #3.
-//       Task #9 created at 635116412924607584 on thread #4.
+// The example displays output similar to the following:
+//    N=100, Total = 110,081,653,682, Mean = 1,100,816,536.82
