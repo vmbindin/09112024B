17f4f5e3e0146db3eb8a59848c8c2692c2c124db
Muhammad Burhan Bin Din (Centific Technologies inc) <v-mbindin@microsoft.com>
2024-11-09 23:41:26 +0800

    Example Task Part 4

--- Program.cs
+++ Program.cs
@@ -1,39 +1,34 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace TaskIntro;
-
-class CustomData
+public class Result
 {
-    public long CreationTime;
-    public int Name;
-    public int ThreadNum;
-}
+   public static void Main()
+   {
+        Task<Double>[] taskArray = { Task<Double>.Factory.StartNew(() => DoComputation(1.0)),
+                                     Task<Double>.Factory.StartNew(() => DoComputation(100.0)),
+                                     Task<Double>.Factory.StartNew(() => DoComputation(1000.0)) };
 
-public class AsyncState
-{
-    public static void Main()
-    {
-        Task[] taskArray = new Task[10];
-        for (int i = 0; i < taskArray.Length; i++)
-        {
-            taskArray[i] = Task.Factory.StartNew((Object? obj) =>
-            {
-                CustomData? data = obj as CustomData;
-                if (data == null) return;
+        var results = new Double[taskArray.Length];
+        Double sum = 0;
 
-                data.ThreadNum = Thread.CurrentThread.ManagedThreadId;
-            },
-            new CustomData() { Name = i, CreationTime = DateTime.Now.Ticks });
+        for (int i = 0; i < taskArray.Length; i++) {
+            results[i] = taskArray[i].Result;
+            Console.Write("{0:N1} {1}", results[i],
+                              i == taskArray.Length - 1 ? "= " : "+ ");
+            sum += results[i];
         }
-        Task.WaitAll(taskArray);
-        foreach (var task in taskArray)
-        {
-            var data = task.AsyncState as CustomData;
-            if (data != null)
-                Console.WriteLine("Task #{0} created at {1}, ran on thread #{2}.",
-                                  data.Name, data.CreationTime, data.ThreadNum);
-        }
-    }
+        Console.WriteLine("{0:N1}", sum);
+   }
+
+   private static Double DoComputation(Double start)
+   {
+      Double sum = 0;
+      for (var value = start; value <= start + 10; value += .1)
+         sum += value;
+
+      return sum;
+   }
 }
+// The example displays the following output:
+//        606.0 + 10,605.0 + 100,495.0 = 111,706.0
